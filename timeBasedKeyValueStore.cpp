e14241a5f452c76a0073b5b2848364bee09023b0
MINAL TANDEKAR <110032697+Meyuki-hell@users.noreply.github.com>
2023-07-26 15:21:15 +0530

    Create timeBasedKeyValueStore.cpp
    using MAP of MAP

---
+++ timeBasedKeyValueStore.cpp
@@ -0,0 +1,32 @@
+/*
+    Your Timebased object will be instantiated and called as such:
+
+    Timebased* obj = new Timebased();
+    obj->setKey(key,value,timestamp);
+    string ans = obj->getValue(targetKey,targetTimestamp);
+*/
+#include<bits/stdc++.h>
+class Timebased {
+    public:
+        map <string, map< int, string , greater<int> > > mp;
+       
+        Timebased() {
+            // Intialise your data structure here.
+             
+        }
+
+    void setKey(string key, string val, int timestamp) {
+        // Write your code here.
+        mp[key][timestamp] = val;
+    }
+
+    string getValue(string targetKey, int targetTimestamp) {
+        // Write your code here.
+        if(mp.count(targetKey) == 0) return "-1";
+        auto it = mp[targetKey].lower_bound(targetTimestamp);
+        if(it == mp[targetKey].end()) return "-1";
+
+        return it -> second;
+     
+    }
+};
